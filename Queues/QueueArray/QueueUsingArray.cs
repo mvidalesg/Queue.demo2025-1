8588772a773ed9444d007d45d64b93bcc1165a31
mvidalesg <miltonvidales77758@correo.itm.edu.co>
2025-03-31 23:16:37 -0500

    Demo using array

---
+++ Queues/QueueArray/QueueUsingArray.cs
@@ -0,0 +1,46 @@
+﻿namespace QueueArray;
+
+public class QueueUsingArray<T>
+{
+    private T[] _elements;
+    private int _front;
+    private int _rear;
+    private int _count;
+
+    public QueueUsingArray(int capacity)
+    {
+        _elements = new T[capacity];
+        _front = 0;
+        _rear = -1;
+        _count = 0;
+    }
+    public void Enqueue(T item)
+    {
+        if (_count == _elements.Length)
+        {
+            throw new InvalidOperationException("Queue is full");
+        }
+        _rear = (_rear + 1) % _elements.Length;
+        _elements[_rear] = item;
+        _count++;
+    }
+    public T Dequeue()
+    {
+        if (_count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        var item = _elements[_front];
+        _front = (_front + 1) % _elements.Length;
+        _count--;
+        return item;
+    }
+    public T Peek()
+    {
+        if (_count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+        return _elements[_front];
+    }
+}
---
+++ Queues/Queues.Ui.Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using QueueArray;
+
+
+var queue = new QueueUsingArray<int>(5);
+var opc = "0";
+do 
+{
+    opc = Menu();
+    switch (opc) 
+    {
+        case "1":
+            try
+            {
+                Console.Write("Enter a number: ");
+                var number = int.Parse(Console.ReadLine()!);
+                queue.Enqueue(number);
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+
+        case "2":
+
+            try
+            {
+                var item = queue.Dequeue();
+                Console.WriteLine($"Item dequeued: {item}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+
+            case "3":
+            try
+            {
+                
+                Console.WriteLine($"Item peek: {queue.Peek()}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            break;
+
+    }
+} while (opc != "0");
+string Menu()
+{
+    Console.WriteLine("1. Enqueue");
+    Console.WriteLine("2. Dequeue");
+    Console.WriteLine("3. Peek");
+    Console.WriteLine("0. Exit");
+    Console.Write("Choose an option: ");
+    return  Console.ReadLine()!;
+}
